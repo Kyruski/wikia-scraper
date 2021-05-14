c766d863f8040194710b70d4ecb64d1bfa9370d0
kyruski <jamescboyett@gmail.com>
2021-05-13 22:58:29 -0700

    Adds grabbing the most recent in-era edit and creating a dictionary with it

--- grabAllPages.js
+++ grabAllPages.js
@@ -3,12 +3,31 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const { JSDOM } = jsdom;
 
-const allPages = [];
+const allPages = {};
 const navClass = 'mw-allpages-nav';
 const bodyClass = 'mw-allpages-body';
+const historyID = "ca-history"
+const historyLinkClass = "mw-changeslist-date";
 const baseURL = 'https://ffxiclopedia.fandom.com';
 const startingURL = 'https://ffxiclopedia.fandom.com/wiki/Special:AllPages';
 const nextPageRegExp = /^[Nn]ext [Pp]age/;
+const cutOffDate = new Date(2010, 02, 23);
+let failedFetches = [];
+
+const months = {
+  'January': 00,
+  'February': 01,
+  'March': 02,
+  'April': 03,
+  "May,": 04,
+  'June': 05,
+  'July': 06,
+  'August': 07,
+  'September': 08,
+  'October': 09,
+  'November': 10,
+  'December': 11
+}
 
 const setPage = async (url) => {
   const response = await fetch(url);
@@ -18,7 +37,6 @@ const setPage = async (url) => {
 
 const scrapePages = async (url) => {
   url = url;
-  let count = 5;
   while (url) {
     let pageDom = await setPage(url)
     let navContainer = await pageDom.window.document.getElementsByClassName(navClass);
@@ -33,27 +51,58 @@ const scrapePages = async (url) => {
       }
     }
     for  (let el of bodyLinks) {
-      let output = {
-        title: el.innerHTML,
-        url: el.href
+      try {
+        const checkOldLink = await grabOldPageLink(el.href);
+        console.log(checkOldLink);
+        if (checkOldLink) {
+          allPages[el.href] = checkOldLink;
+        }
+      } catch (err) {
+        console.log(err);
+        failedFetches.push(el.href);
       }
-      allPages.push(output);
-      // fs.appendFile('./output/allPages.csv')
     }
-    console.log(nextPage);
+    console.log('NEW PAGE!!!! ', nextPage);
     if (nextPage) {
       url = baseURL + nextPage;
     } else url = null;
   }
-  fs.appendFile('./output/allPages.json', JSON.stringify({
-    "allPages": allPages
-  }), (err) => {
+  while (failedFetches.length) {
+    let newFailedFetches = []
+    for  (let el of failedFetches) {
+      try {
+        const checkOldLink = await grabOldPageLink(el.href);
+        if (checkOldLink) {
+          allPages[el.href] = checkOldLink;
+        }
+        
+      } catch (err) {
+        console.log(err);
+        newFailedFetches.push(el.href);
+      }
+    }
+    failedFetches = newFailedFetches;
+  }
+  fs.appendFile('./output/allPages.json', JSON.stringify(allPages), (err) => {
     if (err) {
       throw err;
     };
     console.log('done');
   })
   
+}
+
+const grabOldPageLink = async(url) => {
+  const pageDom = await setPage(baseURL + url + '?offset=&limit=500&action=history')
+  const historyList = await pageDom.window.document.getElementsByClassName(historyLinkClass);
+  for (let el of historyList) {
+    let pageDate = el.innerHTML.replace(',', '').replace(':', ' ').split(' ');
+    let compareDate = new Date(pageDate[4], months[pageDate[3]], pageDate[2], pageDate[0], pageDate[1]);
+    if (cutOffDate > compareDate) {
+      return el.href;
+    }
+  }
+  return null;
 }
 
 scrapePages(startingURL);
